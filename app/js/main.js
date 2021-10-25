11504fd237579194d5587c3a85791312ae91b23d
Artem Tebenkov <artem.tebenkov@icloud.com>
2021-10-25 14:59:45 +0400

    Add products section

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,14 @@ $(function () {
         fade: true,
         autoplay: true,
         autoplaySpeed: 3000
+    });
 
-    })
-
+    $('.star').rateYo({
+        starWidth: '17px',
+        normalFill: '#ccccce',
+        ratedFill: '#ffc35b',
+        readOnly: true,
+        starSvg: '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>'
+    });
 
 });
